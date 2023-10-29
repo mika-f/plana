df752ed048f801ef9032f8016b4aa072267ac1fd
MOCHIZUKI Natsune <me@natsuneko.cat>
2023-10-29 14:31:28 +0900

    feat: add cli

---
+++ src/UdonObfuscator.CLI/CommandLineOptions.cs
@@ -0,0 +1,44 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using System.CommandLine;
+
+using UdonObfuscator.Logging;
+
+namespace UdonObfuscator.CLI;
+
+internal class CommandLineOptions
+{
+    private readonly List<Option> _options;
+
+    public Option<FileInfo> Workspace { get; } = new Option<FileInfo>("--workspace", "path to workspace .csproj or .sln").ExistingOnly();
+
+    public Option<bool> Write { get; } = new ("--write", () => false, "write obfuscated source code in place");
+
+    public Option<bool> DryRun { get; } = new("--dry-run", () => false, "dry-run obfuscate");
+
+    public Option<LogLevel> LogLevel { get; } = new("--log-level", () => Logging.LogLevel.Normal, "log detail level");
+
+    public CommandLineOptions()
+    {
+        _options = [Workspace, Write, DryRun, LogLevel];
+    }
+
+    public void AddOption<T>(string name, string description)
+    {
+        _options.Add(new Option<T>(name, description));
+    }
+
+    public void AddOption<T>(string name, string description, Func<T> getDefaultValue)
+    {
+        _options.Add(new Option<T>(name, getDefaultValue, description));
+    }
+
+    public void Bind(Command command)
+    {
+        foreach (var option in _options)
+            command.AddOption(option);
+    }
+}
---
+++ src/UdonObfuscator.CLI/Program.cs
@@ -0,0 +1,49 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
+using UdonObfuscator;
+using UdonObfuscator.CLI;
+using UdonObfuscator.Logging;
+
+var app = new RootCommand("UdonObfuscator: An obfuscator for VRChat");
+var options = new CommandLineOptions();
+
+options.Bind(app);
+app.SetHandler(Handler);
+
+return await app.InvokeAsync(args);
+
+async Task Handler(InvocationContext context)
+{
+    var workspace = context.ParseResult.GetValueForOption(options.Workspace) ?? throw new InvalidOperationException();
+    var level = context.ParseResult.GetValueForOption(options.LogLevel);
+    var logger = new Logger(level);
+
+    var obfuscator = new Obfuscator(logger);
+    var ret = await obfuscator.ObfuscateAsync(workspace);
+
+    var isDryRun = context.ParseResult.GetValueForOption(options.DryRun);
+    if (isDryRun)
+    {
+        foreach (var (path, content) in ret)
+        {
+            Console.WriteLine(path);
+            Console.WriteLine(content);
+            Console.WriteLine();
+        }
+
+        return;
+    }
+
+    var write = context.ParseResult.GetValueForOption(options.Write);
+    foreach (var (path, content) in ret)
+    {
+        var to = write ? path : Path.Combine(Path.GetDirectoryName(path)!, $"{Path.GetFileNameWithoutExtension(path)}.g.cs");
+        await File.WriteAllTextAsync(to, content);
+    }
+}
