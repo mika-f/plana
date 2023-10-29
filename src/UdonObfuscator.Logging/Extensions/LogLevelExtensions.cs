a2df6792b053e9251d6a7a2226b206441c81d384
MOCHIZUKI Natsune <me@natsuneko.cat>
2023-10-29 14:27:29 +0900

    feat: add logger

---
+++ src/UdonObfuscator.Logging/Extensions/LogLevelExtensions.cs
@@ -0,0 +1,34 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+namespace UdonObfuscator.Logging.Extensions;
+
+public static class LogLevelExtensions
+{
+    public static bool IsDebugEnabled(this LogLevel logLevel)
+    {
+        return logLevel >= LogLevel.Verbose;
+    }
+
+    public static bool IsInfoEnabled(this LogLevel logLevel)
+    {
+        return logLevel >= LogLevel.Normal;
+    }
+
+    public static bool IsWarnEnabled(this LogLevel logLevel)
+    {
+        return logLevel >= LogLevel.Warn;
+    }
+
+    public static bool IsErrorEnabled(this LogLevel logLevel)
+    {
+        return logLevel >= LogLevel.Error;
+    }
+
+    public static bool IsFatalEnabled(this LogLevel logLevel)
+    {
+        return logLevel >= LogLevel.Silent;
+    }
+}
---
+++ src/UdonObfuscator.Logging/Logger.cs
@@ -0,0 +1,49 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using System.Diagnostics;
+
+using UdonObfuscator.Logging.Extensions;
+
+namespace UdonObfuscator.Logging;
+
+public class Logger(LogLevel level)
+{
+    [Conditional("DEBUG")]
+    public void LogDebugInternal(string message)
+    {
+        Debug.WriteLine(message);
+    }
+
+    public void LogDebug(string message)
+    {
+        if (level.IsDebugEnabled())
+            Debug.WriteLine(message);
+    }
+
+    public void LogInfo(string message)
+    {
+        if (level.IsInfoEnabled())
+            Console.WriteLine(message);
+    }
+
+    public void LogWarn(string message)
+    {
+        if (level.IsWarnEnabled())
+            Console.WriteLine(message);
+    }
+
+    public void LogError(string message)
+    {
+        if (level.IsErrorEnabled())
+            Console.WriteLine(message);
+    }
+
+    public void LogFatal(string message)
+    {
+        if (level.IsFatalEnabled())
+            Console.WriteLine(message);
+    }
+}
