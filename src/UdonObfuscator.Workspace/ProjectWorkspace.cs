d6dc8bfa1adad0937bcd057e9d3bcbad483d80e6
MOCHIZUKI Natsune <me@natsuneko.cat>
2023-10-31 19:37:21 +0900

    feat: add workspace abstractions

---
+++ src/UdonObfuscator.Workspace/ProjectWorkspace.cs
@@ -0,0 +1,10 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using UdonObfuscator.Workspace.Abstractions;
+
+namespace UdonObfuscator.Workspace;
+
+public class ProjectWorkspace(FileInfo csproj) : IWorkspace { }
