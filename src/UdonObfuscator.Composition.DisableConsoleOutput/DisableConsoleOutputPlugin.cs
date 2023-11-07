dfff82fb72f51278e26cf799bc323719fde67f74
MOCHIZUKI Natsune <me@natsuneko.cat>
2023-11-07 21:31:25 +0900

    feat: disable console output

---
+++ src/UdonObfuscator.Composition.DisableConsoleOutput/DisableConsoleOutputPlugin.cs
@@ -0,0 +1,68 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using UdonObfuscator.Composition.Abstractions.Algorithm;
+using UdonObfuscator.Composition.Abstractions.Analysis;
+using UdonObfuscator.Composition.Abstractions.Attributes;
+
+namespace UdonObfuscator.Composition.DisableConsoleOutput;
+
+[ObfuscatorAlgorithm("disable-console-output")]
+public class DisableConsoleOutputPlugin : IObfuscatorAlgorithm
+{
+    public IReadOnlyCollection<IObfuscatorAlgorithmOption> Options => new List<IObfuscatorAlgorithmOption>();
+
+    public string Name => "Disable Console Output";
+
+    public void BindParameters(IObfuscatorParameterBinder binder)
+    {
+        // Nothing to do
+    }
+
+    public async Task ObfuscateAsync(List<IProject> projects, CancellationToken ct)
+    {
+        foreach (var document in projects.SelectMany(w => w.Documents))
+        {
+            var log = document.SemanticModel.Compilation.GetTypeByMetadataName("UnityEngine.Debug")!;
+            var oldNode = await document.SyntaxTree.GetRootAsync(ct);
+
+            // one class one document
+
+            foreach (var @class in oldNode.DescendantNodes().OfType<ClassDeclarationSyntax>())
+            {
+                var invocations = @class.DescendantNodes()
+                                        .OfType<InvocationExpressionSyntax>()
+                                        .Where(w => IsReceiverIsUnityEngineDebug(document.SemanticModel, w, log))
+                                        .Select(w => document.SemanticModel.GetSymbolInfo(w).Symbol!)
+                                        .ToList();
+
+                if (invocations.Count != 0)
+                {
+                    var rewriter = new CSharpSyntaxProcessor(document, invocations);
+                    var newNode = (CSharpSyntaxNode)rewriter.Visit(oldNode);
+                    var newTree = CSharpSyntaxTree.Create(newNode, document.SyntaxTree.Options, document.SyntaxTree.FilePath, document.SyntaxTree.Encoding);
+
+                    await document.WriteSyntaxTreeAsync(newTree, ct);
+                }
+            }
+        }
+    }
+
+    private bool IsReceiverIsUnityEngineDebug(SemanticModel sm, InvocationExpressionSyntax invocation, INamedTypeSymbol t)
+    {
+        var si = sm.GetSymbolInfo(invocation);
+        if (si.Symbol is IMethodSymbol symbol)
+        {
+            var receiver = symbol.ReceiverType;
+            return receiver?.Equals(t, SymbolEqualityComparer.Default) ?? false;
+        }
+
+        return false;
+    }
+}
