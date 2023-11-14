89f314587ae1c460fdacd7afb2e5ec3c8c642b86
MOCHIZUKI Natsune <me@natsuneko.cat>
2023-11-14 22:24:46 +0900

    feat: add a control with the same role as System.Windows.Controls.ContentControl in WPF

---
+++ src/UdonObfuscator.Integrations.Unity/Assets/NatsunekoLaboratory/UdonObfuscator/Editor/Components/Abstractions/ContentControl.cs
@@ -0,0 +1,19 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using UnityEngine.UIElements;
+
+namespace NatsunekoLaboratory.UdonObfuscator.Components.Abstractions
+{
+    internal class ContentControl : Control
+    {
+        protected ContentControl(StyledComponents sc) : base(sc)
+        {
+            contentContainer = QuerySelector<VisualElement>("[name='container']");
+        }
+
+        public override VisualElement contentContainer { get; }
+    }
+}
