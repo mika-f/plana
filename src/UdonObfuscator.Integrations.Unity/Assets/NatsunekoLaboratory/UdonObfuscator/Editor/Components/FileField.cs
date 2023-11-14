2ad06692903b103d1110e1e5c7028e217dcd78d5
MOCHIZUKI Natsune <me@natsuneko.cat>
2023-11-14 22:32:42 +0900

    feat: add FileField and DirectoryField

---
+++ src/UdonObfuscator.Integrations.Unity/Assets/NatsunekoLaboratory/UdonObfuscator/Editor/Components/FileField.cs
@@ -0,0 +1,77 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+
+using NatsunekoLaboratory.UdonObfuscator.Components.Abstractions;
+
+using UnityEditor;
+using UnityEditor.UIElements;
+
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace NatsunekoLaboratory.UdonObfuscator.Components
+{
+    internal class FileField : Control
+    {
+        private readonly ObjectField _field;
+
+        public FileInfo Path { get; set; }
+
+        public string Label
+        {
+            get => _field.label;
+            set => _field.label = value;
+        }
+
+        public FileField() : base(StyledComponents.Create("0ba11867e8d00b84882b0bade54f787a", "37dbb67b6aa66684a99564da5d186b54"))
+        {
+            _field = QuerySelector<ObjectField>();
+            _field.objectType = typeof(DefaultAsset);
+            _field.RegisterValueChangedCallback(OnValueChanged);
+        }
+
+        private void OnValueChanged(ChangeEvent<Object> e)
+        {
+            var obj = e.newValue as DefaultAsset;
+            if (obj == null)
+                _field.SetValueWithoutNotify(null);
+
+            var path = AssetDatabase.GetAssetPath(obj);
+
+            try
+            {
+                var attr = File.GetAttributes(path);
+                if (attr.HasFlag(FileAttributes.Directory))
+                    _field.SetValueWithoutNotify(null);
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        public new class UxmlFactory : UxmlFactory<FileField, UxmlTraits> { }
+
+        public new class UxmlTraits : VisualElement.UxmlTraits
+        {
+            private readonly UxmlStringAttributeDescription _label = new UxmlStringAttributeDescription { name = "label", defaultValue = "" };
+
+            public override IEnumerable<UxmlChildElementDescription> uxmlChildElementsDescription
+            {
+                get { yield break; }
+            }
+
+            public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+            {
+                base.Init(ve, bag, cc);
+
+                ((FileField)ve).Label = _label.GetValueFromBag(bag, cc);
+            }
+        }
+    }
+}
