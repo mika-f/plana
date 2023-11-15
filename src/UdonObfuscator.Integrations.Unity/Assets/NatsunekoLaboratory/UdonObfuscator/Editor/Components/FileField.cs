8a9d60a4b6580074d3f821643079ab58f18ecfb8
MOCHIZUKI Natsune <me@natsuneko.cat>
2023-11-15 23:55:49 +0900

    feat: support type property

--- src/UdonObfuscator.Integrations.Unity/Assets/NatsunekoLaboratory/UdonObfuscator/Editor/Components/FileField.cs
+++ src/UdonObfuscator.Integrations.Unity/Assets/NatsunekoLaboratory/UdonObfuscator/Editor/Components/FileField.cs
@@ -3,24 +3,26 @@
 //  Licensed under the MIT License. See LICENSE in the project root for license information.
 // ------------------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 using NatsunekoLaboratory.UdonObfuscator.Components.Abstractions;
 
 using UnityEditor;
 using UnityEditor.UIElements;
 
-using UnityEngine;
 using UnityEngine.UIElements;
+
+using Object = UnityEngine.Object;
 
 namespace NatsunekoLaboratory.UdonObfuscator.Components
 {
     internal class FileField : Control
     {
         private readonly ObjectField _field;
-
-        public FileInfo Path { get; set; }
+        private Type _t;
 
         public string Label
         {
@@ -28,10 +30,24 @@ namespace NatsunekoLaboratory.UdonObfuscator.Components
             set => _field.label = value;
         }
 
+        public string Filter { get; set; }
+
+        public Type Type
+        {
+            get => _t;
+            set
+            {
+                if (_t == value)
+                    return;
+
+                _field.objectType = value;
+                _t = value;
+            }
+        }
+
         public FileField() : base(StyledComponents.Create("0ba11867e8d00b84882b0bade54f787a", "37dbb67b6aa66684a99564da5d186b54"))
         {
             _field = QuerySelector<ObjectField>();
-            _field.objectType = typeof(DefaultAsset);
             _field.RegisterValueChangedCallback(OnValueChanged);
         }
 
@@ -48,6 +64,14 @@ namespace NatsunekoLaboratory.UdonObfuscator.Components
                 var attr = File.GetAttributes(path);
                 if (attr.HasFlag(FileAttributes.Directory))
                     _field.SetValueWithoutNotify(null);
+
+                var dir = Path.GetDirectoryName(path) ?? throw new InvalidOperationException();
+                var entries = Directory.GetFiles(dir, Filter, SearchOption.TopDirectoryOnly);
+
+                if (entries.Contains(path))
+                    return;
+
+                _field.SetValueWithoutNotify(null);
             }
             catch
             {
@@ -59,7 +83,9 @@ namespace NatsunekoLaboratory.UdonObfuscator.Components
 
         public new class UxmlTraits : VisualElement.UxmlTraits
         {
+            private readonly UxmlStringAttributeDescription _filter = new UxmlStringAttributeDescription { name = "filter" };
             private readonly UxmlStringAttributeDescription _label = new UxmlStringAttributeDescription { name = "label", defaultValue = "" };
+            private readonly UxmlStringAttributeDescription _type = new UxmlStringAttributeDescription { name = "type", defaultValue = "" };
 
             public override IEnumerable<UxmlChildElementDescription> uxmlChildElementsDescription
             {
@@ -71,6 +97,10 @@ namespace NatsunekoLaboratory.UdonObfuscator.Components
                 base.Init(ve, bag, cc);
 
                 ((FileField)ve).Label = _label.GetValueFromBag(bag, cc);
+                ((FileField)ve).Filter = _filter.GetValueFromBag(bag, cc);
+
+                var t = _filter.GetValueFromBag(bag, cc);
+                ((FileField)ve).Type = Type.GetType(t) ?? typeof(DefaultAsset);
             }
         }
     }
