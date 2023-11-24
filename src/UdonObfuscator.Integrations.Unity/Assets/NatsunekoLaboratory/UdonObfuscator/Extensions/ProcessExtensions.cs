6527a95658ee4727ec7e9720f396a239290e2eab
MOCHIZUKI Natsune <me@natsuneko.cat>
2023-11-24 19:52:31 +0900

    feat: support to run external process as async

---
+++ src/UdonObfuscator.Integrations.Unity/Assets/NatsunekoLaboratory/UdonObfuscator/Extensions/ProcessExtensions.cs
@@ -0,0 +1,28 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace NatsunekoLaboratory.UdonObfuscator.Extensions
+{
+    internal static class ProcessExtensions
+    {
+        public static Task<int> RunAsync(this Process obj)
+        {
+            var exit = new TaskCompletionSource<int>();
+
+            obj.StartInfo.RedirectStandardOutput = true;
+            obj.Exited += (sender, args) => exit.TrySetResult(obj.ExitCode);
+
+            var ret = obj.Start();
+            if (ret)
+                return exit.Task;
+
+            throw new InvalidOperationException();
+        }
+    }
+}
