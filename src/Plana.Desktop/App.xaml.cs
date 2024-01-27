2ecbd8b25db252b839cea4cecf4a8f8c55757070
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-01-27 19:46:38 +0900

    feat: set window title and default sizing

--- src/Plana.Desktop/App.xaml.cs
+++ src/Plana.Desktop/App.xaml.cs
@@ -8,5 +8,19 @@
 
             MainPage = new MainPage();
         }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Title = "Plana for Desktop";
+
+            // workaround until release .NET 9
+            // set default window size for MAUI
+            // ref: https://github.com/dotnet/maui/issues/7592
+            window.Width = 1280;
+            window.Height = 720;
+
+            return window;
+        }
     }
 }
