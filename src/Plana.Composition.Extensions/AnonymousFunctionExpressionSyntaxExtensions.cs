9a8cd7fce812a59b7b8750e24a3f036b3910b682
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-02-11 00:27:26 +0900

    feat: support to check node has the modifier

---
+++ src/Plana.Composition.Extensions/AnonymousFunctionExpressionSyntaxExtensions.cs
@@ -0,0 +1,17 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Plana.Composition.Extensions;
+
+public static class AnonymousFunctionExpressionSyntaxExtensions
+{
+    public static bool HasModifier(this AnonymousFunctionExpressionSyntax node, SyntaxToken modifier)
+    {
+        return node.Modifiers.Any(w => w.IsEquivalentTo(modifier));
+    }
+}
