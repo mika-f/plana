eb5bac9e9d2358a0179419a40eae09374843bc0b
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-02-14 20:43:45 +0900

    fix: fix compile error

---
+++ src/Plana.Testing/InlineSymbol.cs
@@ -0,0 +1,16 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using Microsoft.CodeAnalysis;
+
+namespace Plana.Testing;
+
+public class InlineSymbol : ITestableObject<ISymbol>
+{
+    public Task ToMatchInlineSnapshot(ISymbol snapshot)
+    {
+        throw new NotImplementedException();
+    }
+}
