7de5174f3b2d28833268244ec820b650c4ed8cc6
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-02-19 22:23:14 +0900

    feat: Type to INamedTypeSymbol

---
+++ src/Plana.Composition.Extensions/TypeExtensions.cs
@@ -0,0 +1,16 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using Microsoft.CodeAnalysis;
+
+namespace Plana.Composition.Extensions;
+
+public static class TypeExtensions
+{
+    public static INamedTypeSymbol? ToSymbol(this Type t, SemanticModel sm)
+    {
+        return sm.Compilation.GetTypeByMetadataName(t.FullName ?? throw new InvalidOperationException());
+    }
+}
