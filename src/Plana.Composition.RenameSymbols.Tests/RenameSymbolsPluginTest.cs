b17e4b79e9d3cd23ebc2bc66ad5b906ddda0a15a
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-02-20 21:20:57 +0900

    chore: add tests

--- src/Plana.Composition.RenameSymbols.Tests/RenameSymbolsPluginTest.cs
+++ src/Plana.Composition.RenameSymbols.Tests/RenameSymbolsPluginTest.cs
@@ -3,8 +3,13 @@
 //  Licensed under the MIT License. See LICENSE in the project root for license information.
 // ------------------------------------------------------------------------------------------
 
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
+using Plana.Composition.Abstractions;
+using Plana.Composition.Abstractions.Analysis;
+using Plana.Composition.Extensions;
 using Plana.Testing;
 
 namespace Plana.Composition.RenameSymbols.Tests;
@@ -60,8 +65,7 @@ public class RenameSymbolsPluginTest
 
         var abstraction = await container.GetSourceByPathAsync("Plana.Composition.Abstractions/IPlanaPluginRunContext.cs");
         var implementation = await container.GetSourceByPathAsync("Plana/PlanaPluginRunContext.cs");
-        var reference1 = await container.GetSourceByPathAsync("Plana.Composition.DisableConsoleOutput/DisableConsoleOutputPlugin.cs");
-        var reference2 = await container.GetSourceByPathAsync("Plana/Obfuscator.cs");
+        var reference = await container.GetSourceByPathAsync("Plana.Composition.DisableConsoleOutput/DisableConsoleOutputPlugin.cs");
 
         // Solution -> _0xc7b29ba1
         const string identifier = "_0xc7b29ba1";
@@ -72,10 +76,29 @@ public class RenameSymbolsPluginTest
         var solutionImplDecl = await implementation.GetFirstSyntax<PropertyDeclarationSyntax>();
         Assert.Equal(identifier, solutionImplDecl.Identifier.ToString());
 
-        /*
-        var solutionRef1 = await reference1.GetSyntax<MemberAccessExpressionSyntax>();
+        bool IsAccessToContextSolution(MemberAccessExpressionSyntax syntax, SemanticModel sm)
+        {
+            if (syntax.Expression is not IdentifierNameSyntax receiver)
+                return false;
 
-        var solutionRef2 = await reference2.GetSyntax<MemberAccessExpressionSyntax>();
-        */
+            // context is local parameter and IPlanaPluginRunContext
+            var context = sm.GetSymbolInfo(receiver).Symbol;
+            if (context is not IParameterSymbol s1)
+                return false;
+
+            var a = s1.Type.Equals(typeof(IPlanaPluginRunContext).ToSymbol(sm), SymbolEqualityComparer.Default);
+
+// accessed property is IPlanaSolution
+            var ccc = sm.GetSymbolInfo(syntax.Name);
+            if (ccc.Symbol is not IPropertySymbol s2)
+                return false;
+
+            var b = s2.Type.Equals(typeof(ISolution).ToSymbol(sm), SymbolEqualityComparer.Default);
+
+            return a && b;
+        }
+
+        var solutionRef = await reference.GetFirstSyntax<MemberAccessExpressionSyntax>(IsAccessToContextSolution);
+        Assert.Equal(identifier, solutionRef.Name.ToString());
     }
 }
